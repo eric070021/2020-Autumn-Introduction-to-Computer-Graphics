4c1c62e08ea0a5b9cb682aa3f24f23fa536a5fd3
Eric Hsiao <80262378+eric070021@users.noreply.github.com>
2022-06-15 17:53:10 +0800

    Add files via upload

---
+++ HW4/CG_hw4/build/CMakeFiles/glew_dll.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glew_dll"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/glew_dll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
