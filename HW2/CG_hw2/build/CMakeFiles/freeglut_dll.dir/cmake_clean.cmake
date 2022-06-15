f3adf19fcd162d029be8aad49ae70098b5049cc6
Eric Hsiao <80262378+eric070021@users.noreply.github.com>
2022-06-16 00:41:17 +0800

    Delete HW2 directory

--- HW2/CG_hw2/build/CMakeFiles/freeglut_dll.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/freeglut_dll"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/freeglut_dll.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- HW2/readme.txt
+++
@@ -1,5 +0,0 @@
-Need to include:
-1.freeglut
-2.FreeImage-3.18
-3.glew-2.2.0
-4.glm
