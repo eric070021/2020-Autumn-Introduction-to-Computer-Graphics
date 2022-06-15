694a9fb6cec071dc8b44c15eaa601a0483d725fe
Eric Hsiao <80262378+eric070021@users.noreply.github.com>
2022-06-16 00:41:28 +0800

    Delete HW3 directory

--- HW3/CG_hw3/build/CMakeFiles/FreeImage_dll.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/FreeImage_dll"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/FreeImage_dll.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
